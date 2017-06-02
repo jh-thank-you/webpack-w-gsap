6dadd8f8084b4612d6bcfda6fdd6729fa9403058
jh-thank-you <admin_01a@OSXMain.local>
2017-06-02 02:54:47 -0400

    CSS and HTML cleanup, Event Bus used for Hero IMAGE but has issues (it only works on seconf load)

---
+++ assets/components/nav-button.vue
@@ -0,0 +1,47 @@
+<template>
+    
+	<router-link v-bind:to="'/' + section">
+		<div @click="heroImageSelected" :id="id" class="media-type">
+			<div class="handwritten media-title"><p>{{ section }}</p></div>
+		</div>
+	</router-link>
+
+</template>
+
+
+<script>
+
+	import { eventBus } from 'assets/main.js';
+
+	export default {
+
+		props: [ 'id', 'section', ], // END props
+		data() {
+			return {
+
+				// heroSrc: '',
+			}
+		}, // END data  
+		methods: {
+
+			heroImageSelected() {
+
+				this.heroSrc = this.id;
+				eventBus.$emit('heroImageSelected', this.heroSrc);
+				console.log(this.heroSrc + " = this.heroSrc - nav button clicked");
+			},
+	
+
+		}, // END methods
+
+
+	}; // END export default
+
+</script>
+
+
+
+<style scoped>
+
+
+</style>
---
+++ assets/sections/video.vue
@@ -0,0 +1,39 @@
+<template>
+	<div id="section-wrap" class="texture-paper-bkg">
+
+		<button-close-section-raster></button-close-section-raster>
+
+	    <hero-image></hero-image>
+	        	
+	    <examples-list-video></examples-list-video>
+		
+	</div>
+</template>
+
+
+<script>
+
+	import buttonCloseSectionRaster from 'components/button-close-section-raster.vue';
+	import heroImage from 'components/hero-image.vue';
+	// import modalButton from 'components/modal-button.vue';
+	import examplesListVideo from 'components/examples-list-video.vue';
+
+	export default {
+
+		components: {
+
+			buttonCloseSectionRaster, 
+			heroImage, 
+			examplesListVideo, 
+			
+		}, // END
+
+	}; // END export default
+
+</script>
+
+
+<style scoped>
+
+
+</style>
