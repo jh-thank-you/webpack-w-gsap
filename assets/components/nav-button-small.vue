a3b767891a3c301c58fc7e527eadc8bf21ab54d2
jh-thank-you <admin_01a@OSXMain.local>
2017-06-16 16:55:44 -0400

    Added nav-small-screen-menu.vue component - includes CSS with media queries.

---
+++ assets/components/nav-button-small.vue
@@ -0,0 +1,37 @@
+<template>
+    
+	<router-link v-bind:to="'/' + section">
+			<div :id="id" class="handwritten media-title"><p>{{ section }}</p></div>
+	</router-link>
+
+</template>
+
+
+<script>
+
+	import { eventBus } from 'assets/main.js';
+
+	export default {
+
+		props: [ 'id', 'section', ], // END props
+		data() {
+			return {
+
+			}
+		}, // END data  
+		methods: {
+
+	
+		}, // END methods
+
+
+	}; // END export default
+
+</script>
+
+
+
+<style scoped>
+
+
+</style>
---
+++ assets/components/nav-small-screen-menu.vue
@@ -0,0 +1,70 @@
+<template>
+
+  <nav class="conrner-nav-small">
+      
+      <div id="name-and-tag-small">
+        <p id="name-title">Hainis Art Direction</p>
+        <p id="tagline">&thinsp;bad name. good work.</p>
+      </div>
+
+    <div class="inner-wrap-fixed-scroll">
+      <div class="inner-wrap">
+        <div class="small-nav-top-spacer"></div>
+
+        <nav-button-small v-for="navigationButton in navigationButtons" v-bind:key="navigationButton.id" :id="navigationButton.id" :section="navigationButton.section"></nav-button-small>
+
+        <div class="small-nav-bottom-spacer"></div>
+      </div>
+    </div>
+
+  </nav>
+
+</template>
+
+
+<script>
+
+  import navButtonSmall from 'components/nav-button-small.vue';
+
+  export default {
+
+    components: {
+     
+      navButtonSmall,
+
+    }, // END components
+    data() {
+      return {
+
+        navigationButtons: [ 
+
+          {id: 'nav-print-small', section: 'PRINT', },
+          {id: 'nav-video-small', section: 'VIDEO', },
+          {id: 'nav-outdoor-small', section: 'OUTDOOR', },
+          {id: 'nav-online-small', section: 'ONLINE', },
+
+        ], // END navButtons
+
+        id: '',
+        section: '',
+
+      }; // END return
+    }, // END data
+
+  }; // END export default
+
+</script>
+
+
+
+<style scoped>
+
+#hero-wrap,
+.hero-image {
+  display: none;
+  background-image: none;
+}
+
+
+
+</style>
