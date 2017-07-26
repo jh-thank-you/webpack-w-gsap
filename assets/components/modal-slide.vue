489692fb87d8ec989db791cfcc30ff78d1932ff0
jh-thank-you <jhainis@yahoo.com>
2017-07-26 11:33:37 -0400

    slideshow image now set as background-image: contain;

--- assets/components/modal-slide.vue
+++ assets/components/modal-slide.vue
@@ -3,14 +3,14 @@
       <div> <!-- START - No class Div Vue Passes the Class to Containing Div -->
         <div class="center-slide">
 
-            <div class="slide-img-wrap">
-              <img v-if="slide.showImage" :src="slide.src" :alt:="slide.alt">
+            <div v-if="slide.showImage" :style="{ 'background-image': 'url(' + slide.src + ')' }" :alt:="slide.alt" class="slide-img-wrap">
             </div>
 
             <div v-if="!slide.showImage" class="center-slide modal-description">
               <h1 class="handwritten">{{ slide.client }}</h1>
               <p> {{ slide.pText }} </p>
             </div>
+
         </div>
       </div> <!-- END - No class Div Vue Passes the Class to Containing Div -->
 
