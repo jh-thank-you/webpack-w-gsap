bf59b0b6746b843213b0e2e9b3f996b52a655e28
admin_01a <admin_01a@iMac.home>
2017-12-01 20:41:53 -0500

    adding assets.

--- assets/modals/modal-print-ko-fightgear.js
+++ assets/modals/modal-print-ko-fightgear.js
@@ -1,6 +1,39 @@
 let slides = [
   {
     navID:     'slide-1',
+    client:    '',
+    alt:       'KO Fightgear - Facesaver Ad',
+    showImage: true,
+    src:       require('assets/img/modal-slides/modal-print-ko-fightgear-01c.jpg'),
+    showVideo: false,
+    videoSrc:  '',
+    showText:  false,
+    pText:     '',
+  },
+  {
+    navID:     'slide-2',
+    client:    '',
+    alt:       'KO Fightgear - Heavy Bag Ad',
+    showImage: true,
+    src:       require('assets/img/modal-slides/modal-print-ko-fightgear-02.jpg'),
+    showVideo: false,
+    videoSrc:  '',
+    showText:  false,
+    pText:     '',
+  },
+  {
+    navID:     'slide-3',
+    client:    '',
+    alt:       'AKO Fightgear - Tear Drop Heavy Bag Ad',
+    showImage: true,
+    src:       require('assets/img/modal-slides/modal-print-ko-fightgear-03.jpg'),
+    showVideo: false,
+    videoSrc:  '',
+    showText:  false,
+    pText:     '',
+  },
+  {
+    navID:     'slide-4',
     client:    'KO Fightgear',
     alt:       '',
     showImage: false,
@@ -8,7 +41,7 @@ let slides = [
     showVideo: false,
     videoSrc:  '',
     showText:  true,
-    pText:     'ART & COPY TO COME - Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    pText:     'COPY TO COME - Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
 
 
